00a1f471b004da5b2d510f464e510191af2b880d
Dhannyphantom <64708060+Dhannyphantom@users.noreply.github.com>
2022-04-26 21:59:09 +0100

    Creating and Opening a new page

---
+++ src/LaunchPage.java
@@ -0,0 +1,29 @@
+import javax.swing.*;
+import java.awt.event.ActionEvent;
+import java.awt.event.ActionListener;
+
+public class LaunchPage implements ActionListener {
+    JButton button;
+
+    LaunchPage() {
+        button = new JButton();
+        button.setText("Launch New Page");
+        button.setFocusable(false);
+        button.addActionListener(this);
+        button.setBounds(150,100, 100,60);
+
+        JFrame frame = new JFrame();
+        frame.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+        frame.setSize(500,500);
+        frame.setLayout(null);
+        frame.add(button);
+        frame.setVisible(true);
+    }
+
+    @Override
+    public void actionPerformed(ActionEvent e) {
+        if (e.getSource() == button) {
+            new NewPage();
+        }
+    }
+}
