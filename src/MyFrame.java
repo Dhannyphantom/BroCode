8b518c2349d560309e63ddb34e6c05fb8767b65f
Dhannyphantom <64708060+Dhannyphantom@users.noreply.github.com>
2022-04-27 14:31:04 +0100

    JCombobox

--- src/MyFrame.java
+++ src/MyFrame.java
@@ -4,46 +4,28 @@ import java.awt.event.ActionListener;
 
 public class MyFrame extends JFrame implements ActionListener {
     JPanel panel;
-    JRadioButton radio;
-    JRadioButton radio1;
-    JRadioButton radio2;
-    ButtonGroup buttonGroup;
+    JComboBox<String> comboBox;
+
     MyFrame() {
 
-        radio = new JRadioButton("male");
-        radio1 = new JRadioButton("female");
-        radio2 = new JRadioButton("other");
+        String[] animals = {"cat", "dogs", "chicken", "rabbit"};
 
-        radio.addActionListener(this);
-        radio1.addActionListener(this);
-        radio2.addActionListener(this);
-
-        buttonGroup = new ButtonGroup();
-        buttonGroup.add(radio);
-        buttonGroup.add(radio1);
-        buttonGroup.add(radio2);
+        comboBox = new JComboBox<>(animals);
+        comboBox.addItem("goat");
+        comboBox.setEditable(true);
 
         panel = new JPanel();
-        panel.add(radio);
-        panel.add(radio1);
-        panel.add(radio2);
+        panel.add(comboBox);
 
         this.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
         this.setSize(500,500);
         this.add(panel);
-        this.pack();
         this.setVisible(true);
     }
 
     @Override
     public void actionPerformed(ActionEvent e) {
-        if (e.getSource() == radio) {
-            System.out.println("You're a guy");
-        } else if (e.getSource() == radio1) {
-            System.out.println("You're a lady");
-        } else if (e.getSource() == radio2) {
-            System.out.println("You're something else");
-        }
+
 
     }
 }
