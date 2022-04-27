83acedadf024cc8a660eb84ac0672287cc6ee7fa
Dhannyphantom <64708060+Dhannyphantom@users.noreply.github.com>
2022-04-27 20:19:24 +0100

    Drag and Drop, not working though

--- src/MyFrame.java
+++ src/MyFrame.java
@@ -1,76 +1,18 @@
 import javax.swing.*;
-import java.awt.*;
-import java.awt.event.KeyEvent;
-import java.awt.event.KeyListener;
-import java.awt.event.MouseEvent;
-import java.awt.event.MouseListener;
 
-public class MyFrame extends JFrame implements KeyListener, MouseListener {
-    JPanel panel;
+public class MyFrame extends JFrame {
+    DragPanel dragPanel;
     MyFrame() {
 
-        panel = new JPanel();
-        panel.setBackground(Color.orange);
-        panel.setBounds(0,0,100,100);
-        panel.addMouseListener(this);
-        panel.setOpaque(true);
+        dragPanel = new DragPanel();
 
         this.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
         this.setSize(500,500);
         this.setLayout(null);
-        this.add(panel);
-        this.addKeyListener(this);
+        this.add(dragPanel);
         this.setLocationRelativeTo(null);
         this.setVisible(true);
 
     }
 
-    @Override
-    public void keyTyped(KeyEvent e) {
-        switch (e.getKeyChar()) {
-            case 'w' -> panel.setLocation(panel.getX(), panel.getY() - 10);
-            case 'a' -> panel.setLocation(panel.getX() - 10, panel.getY());
-            case 'd' -> panel.setLocation(panel.getX() + 10, panel.getY());
-            case 's' -> panel.setLocation(panel.getX(), panel.getY() + 10);
-        }
-
-    }
-
-    @Override
-    public void keyPressed(KeyEvent e) {
-
-    }
-
-    @Override
-    public void keyReleased(KeyEvent e) {
-
-    }
-
-    @Override
-    public void mouseClicked(MouseEvent e) {
-        panel.setBackground(Color.gray);
-    }
-
-    @Override
-    public void mousePressed(MouseEvent e) {
-        panel.setBackground(Color.magenta);
-
-    }
-
-    @Override
-    public void mouseReleased(MouseEvent e) {
-        panel.setBackground(Color.cyan);
-    }
-
-    @Override
-    public void mouseEntered(MouseEvent e) {
-        panel.setBackground(Color.pink);
-
-    }
-
-    @Override
-    public void mouseExited(MouseEvent e) {
-        panel.setBackground(Color.orange);
-
-    }
 }
