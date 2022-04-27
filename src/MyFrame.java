bd55864166105d8d400227281db03993e1c60080
Dhannyphantom <64708060+Dhannyphantom@users.noreply.github.com>
2022-04-27 19:39:20 +0100

    KeyListeners and MouseListeners

--- src/MyFrame.java
+++ src/MyFrame.java
@@ -1,82 +1,76 @@
 import javax.swing.*;
-import javax.swing.filechooser.FileFilter;
 import java.awt.*;
-import java.awt.event.ActionEvent;
-import java.awt.event.ActionListener;
 import java.awt.event.KeyEvent;
-import java.io.File;
+import java.awt.event.KeyListener;
+import java.awt.event.MouseEvent;
+import java.awt.event.MouseListener;
 
-public class MyFrame extends JFrame implements ActionListener {
-    JMenuBar menuBar;
-    JMenu fileMenu;
-    JMenu editMenu;
-    JMenu helpMenu;
-    JMenuItem newItem;
-    JMenuItem openItem;
-    JMenuItem exitItem;
-    JFileChooser fileChooser;
-
+public class MyFrame extends JFrame implements KeyListener, MouseListener {
+    JPanel panel;
     MyFrame() {
 
-        ImageIcon openIcon = new ImageIcon("assets/favicon.png");
-        menuBar = new JMenuBar();
-
-        fileMenu = new JMenu("File");
-        editMenu = new JMenu("Edit");
-        helpMenu = new JMenu("Help");
-
-        fileMenu.setMnemonic(KeyEvent.VK_F);
-        editMenu.setMnemonic(KeyEvent.VK_E);
-        helpMenu.setMnemonic(KeyEvent.VK_H);
-
-        newItem = new JMenuItem("New");
-        openItem = new JMenuItem("Open");
-        exitItem = new JMenuItem("Exit");
-
-        newItem.setMnemonic(KeyEvent.VK_N);
-        openItem.setMnemonic(KeyEvent.VK_O);
-        exitItem.setMnemonic(KeyEvent.VK_E);
-
-        newItem.addActionListener(this);
-        openItem.addActionListener(this);
-        exitItem.addActionListener(this);
-
-        newItem.setIcon(openIcon);
-
-        menuBar.add(fileMenu);
-        menuBar.add(editMenu);
-        menuBar.add(helpMenu);
-
-        fileMenu.add(newItem);
-        fileMenu.add(openItem);
-        fileMenu.add(exitItem);
+        panel = new JPanel();
+        panel.setBackground(Color.orange);
+        panel.setBounds(0,0,100,100);
+        panel.addMouseListener(this);
+        panel.setOpaque(true);
 
         this.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
         this.setSize(500,500);
-        this.add(menuBar);
-        this.setLayout(new FlowLayout());
+        this.setLayout(null);
+        this.add(panel);
+        this.addKeyListener(this);
+        this.setLocationRelativeTo(null);
         this.setVisible(true);
 
     }
 
     @Override
-    public void actionPerformed(ActionEvent e) {
-        if (e.getSource() == newItem) {
-            System.out.println("New clicked");
+    public void keyTyped(KeyEvent e) {
+        switch (e.getKeyChar()) {
+            case 'w' -> panel.setLocation(panel.getX(), panel.getY() - 10);
+            case 'a' -> panel.setLocation(panel.getX() - 10, panel.getY());
+            case 'd' -> panel.setLocation(panel.getX() + 10, panel.getY());
+            case 's' -> panel.setLocation(panel.getX(), panel.getY() + 10);
         }
-        if (e.getSource() == openItem) {
-            System.out.println("open file clicked");
-            fileChooser = new JFileChooser();
-//            int response = fileChooser.showSaveDialog(null);
-            int response = fileChooser.showOpenDialog(null);
-            if (response == JFileChooser.APPROVE_OPTION) {
-                File file = new File(fileChooser.getSelectedFile().getAbsolutePath());
-                System.out.println(file);
-            }
-        }
-        if (e.getSource() == exitItem) {
-            System.exit(0);
-        }
+
+    }
+
+    @Override
+    public void keyPressed(KeyEvent e) {
+
+    }
+
+    @Override
+    public void keyReleased(KeyEvent e) {
+
+    }
+
+    @Override
+    public void mouseClicked(MouseEvent e) {
+        panel.setBackground(Color.gray);
+    }
+
+    @Override
+    public void mousePressed(MouseEvent e) {
+        panel.setBackground(Color.magenta);
+
+    }
+
+    @Override
+    public void mouseReleased(MouseEvent e) {
+        panel.setBackground(Color.cyan);
+    }
+
+    @Override
+    public void mouseEntered(MouseEvent e) {
+        panel.setBackground(Color.pink);
+
+    }
+
+    @Override
+    public void mouseExited(MouseEvent e) {
+        panel.setBackground(Color.orange);
 
     }
 }
