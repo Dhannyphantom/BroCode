af13c8c0f30daae7c881a2c4ff500f7f2a996314
Dhannyphantom <64708060+Dhannyphantom@users.noreply.github.com>
2022-04-27 13:38:15 +0100

    JCheckBox

--- src/MyFrame.java
+++ src/MyFrame.java
@@ -4,21 +4,28 @@ import java.awt.event.ActionEvent;
 import java.awt.event.ActionListener;
 
 public class MyFrame extends JFrame implements ActionListener {
-    JTextField textField;
-    JButton button;
+    JCheckBox checkBox;
     JPanel panel;
-
+    JButton button;
     MyFrame() {
 
-        button = new JButton();
-        button.setText("Submit");
+        ImageIcon icon1 = new ImageIcon("assets/favicon.png");
+        ImageIcon icon2 = new ImageIcon("assets/x_icon.png");
+
+
+        button = new JButton("Submit");
         button.addActionListener(this);
 
-        textField = new JTextField();
-        textField.setPreferredSize(new Dimension(200,50));
+
+        checkBox = new JCheckBox();
+        checkBox.setText("Are you a robot");
+        checkBox.setFocusable(false);
+        checkBox.setFont(new Font("MV Boli", Font.BOLD, 25));
+//        checkBox.setIcon(icon1);
+//        checkBox.setSelectedIcon(icon2);
 
         panel = new JPanel();
-        panel.add(textField);
+        panel.add(checkBox);
         panel.add(button);
 
         this.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
@@ -31,8 +38,11 @@ public class MyFrame extends JFrame implements ActionListener {
     @Override
     public void actionPerformed(ActionEvent e) {
         if (e.getSource() == button) {
-            System.out.println("Welcome "+ textField.getText());
+            if (checkBox.isSelected()){
+                System.out.println("Yes you are a robot");
+            } else {
+                System.out.println("Of course you're not");
+            }
         }
-
     }
 }
