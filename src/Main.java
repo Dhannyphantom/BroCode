e81c615c6099237bf76f489db9f253bdb6529bce
Dhannyphantom <64708060+Dhannyphantom@users.noreply.github.com>
2022-04-28 20:55:08 +0100

    Generic Methods

--- src/Main.java
+++ src/Main.java
@@ -1,7 +1,25 @@
 
 public class Main  {
     public static void main(String[] args) {
-        new MyFrame();
+
+        Integer[] numbers = {1,2,3,4,5,6};
+        Double[] doubles = {6.6,5.5,4.4,3.3,2.2,1.1};
+        Character[] chars = {'q','w','e','r','t','y'};
+        String[] strings = {"Bro", "Dan", "Pete", "Jake", "Von", "Dex"};
+
+        displayValues(strings);
+        System.out.println(getFirst(chars));
+    }
+
+    public static <T> void displayValues (T[] x) {
+        for (T val : x) {
+            System.out.print(val + " ");
+        }
+        System.out.println();
+    }
+
+    public static <T> T getFirst (T[] x) {
+        return x[0];
     }
 
 }
